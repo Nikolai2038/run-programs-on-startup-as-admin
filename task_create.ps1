98be2b2563daa1563afd34d57d3eea0a33947ed5
Nikolai-415 <nikolai-415@yandex.ru>
2022-11-13 13:36:18 +0300

    Добавил первые наработки

---
+++ task_create.ps1
@@ -0,0 +1,31 @@
+﻿#!/usr/bin/env pwsh
+
+$taskName = "RunProgramsOnStartup"
+
+# Создаём расписание запуска
+$trigger = New-JobTrigger -AtLogOn
+
+# Просим пользователя авторизоваться (для доступа к Планировщику Заданий)
+$credential = Get-Credential
+
+# В качестве опции указываем запуск задания с повышенными привилегиями
+$option = New-ScheduledJobOption -RunElevated -WakeToRun
+
+# Переменную будем передавать в сам скрипт
+$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Снимаем регистрацию с задания, если оно уже существует
+($is_job_already_created = Get-ScheduledJob -Name "$taskName") 2> $null;
+if ($is_job_already_created) {
+    Unregister-ScheduledJob -Name "$taskName" | Sleep 3
+}
+
+# Регистрируем само задание
+Register-ScheduledJob `
+    -Name "$taskName" `
+    -Trigger $trigger `
+    -Credential $credential `
+    -ScheduledJobOption $option `
+    -FilePath "${scriptFolder}/task_script.ps1" `
+    -ArgumentList @("${scriptFolder}")
+    
---
+++ task_script.ps1
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env pwsh
+
+param(
+    [string]$scriptFolder = ""
+)
+# Чтобы скрипт можно было запускать вручную
+if (!($scriptFolder)) {
+    $scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
+}
+$pathToExecutables = "$scriptFolder/executables"
+
+$files = @(Get-ChildItem "$pathToExecutables")
+foreach ($file in $files) {
+    if ($file -match "(.*).lnk") {
+        $fileName = ($file | %{$_.Name})
+        $fileFullPath = "$pathToExecutables/$fileName"
+        Start-Process -FilePath "$fileFullPath" -Verb RunAs
+    }
+}
