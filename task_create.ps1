f6dd58731e40a5a4463882b4d451c17591ea98e3
Nikolai2038 <nikolai2038@yandex.ru>
2023-09-10 19:22:50 +0300

    Allow start if on batteries and some refactoring

--- task_create.ps1
+++ task_create.ps1
@@ -2,19 +2,23 @@
 
 $taskName = "RunProgramsOnStartup"
 
-# Creating a launch schedule
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-
-# We will pass this variable to the script itself
-$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -ExecutionPolicy ByPass & '${scriptFolder}\task_script.ps1' '${scriptFolder}'"
-
 # We remove registration of the task if it already exists
 ($is_job_already_created = Get-ScheduledTask -TaskName "$taskName") 2> $null;
 if ($is_job_already_created) {
     Unregister-ScheduledTask -Confirm:$false -TaskName "$taskName" | Start-Sleep 3
 }
 
+# Creating a launch schedule
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+
+# We will pass this variable to the script itself
+$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Action to execute
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -ExecutionPolicy ByPass & '${scriptFolder}\task_script.ps1' '${scriptFolder}'"
+
+# (for laptops)
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
 # Registration of the task
-Register-ScheduledTask -TaskName "$taskName" -Trigger $trigger -Action $action
+Register-ScheduledTask -TaskName "$taskName" -Trigger $trigger -Action $action -Settings $settings
